f3a4ce5a405e369ce644012581b3c887441ec291
Nguyễn Văn Toản <95391834+vantoan2905@users.noreply.github.com>
2025-08-22 08:32:59 +0700

    Refactor code structure for improved readability and maintainability

--- app_django/asgi.py
+++ app_django/asgi.py
@@ -5,13 +5,13 @@ ASGI config for app_django project.
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from jwt_middleware.jwt_middleware import JwtAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_django.settings")
 
-# Khởi tạo ASGI app cho HTTP
 django_asgi_app = get_asgi_application()
 
+
+from app_django.jwt_middleware.jwt_middleware import JwtAuthMiddleware
 import provision.urls.camera_routing as routing  
 
 application = ProtocolTypeRouter({
