26c47c0c44463ab5288619e9bd8131db0ac9a69c
Nguyễn Văn Toản <95391834+vantoan2905@users.noreply.github.com>
2025-08-27 22:35:15 +0700

    Add YOLO data loader and Faster R-CNN trainer implementation
    - Implemented LoadYoloData class for loading YOLO formatted datasets.
    - Created FasterRCNNTrainer class for training Faster R-CNN models with customizable parameters.
    - Added test scripts and Jupyter notebook for validating data loading and model training.
    - Included example usage and model summary in the test notebook.
    - Established directory structure for data loading and model training.

---
+++ provision/ai_models/test.py
@@ -0,0 +1,39 @@
+import cv2
+from data_loader.load_yolo_data import  LoadYoloData
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+def collate_fn(batch):
+    return tuple(zip(*batch))
+# ===============================
+# 1. Setup paths
+# ===============================
+from torch.utils.data import DataLoader
+
+train_images_path = r"D:\pcb_defect\ProdVision_django\PCB_DATASET_yolo_version\images\train"
+train_labels_path = r"D:\pcb_defect\ProdVision_django\PCB_DATASET_yolo_version\labels\train"
+val_images_path   = r"D:\pcb_defect\ProdVision_django\PCB_DATASET_yolo_version\images\val"
+val_labels_path   = r"D:\pcb_defect\ProdVision_django\PCB_DATASET_yolo_version\labels\val"
+yaml_path = r"D:\pcb_defect\ProdVision_django\PCB_DATASET_yolo_version\data.yaml"
+
+
+print("Loading training data...")
+train_dataset = LoadYoloData(train_images_path, train_labels_path, yaml_path)
+
+print("Loading validation data...")
+val_dataset = LoadYoloData(val_images_path, val_labels_path, yaml_path)
+
+
+
+
+# collate_fn cho object detection
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+val_loader   = DataLoader(val_dataset, batch_size=2, shuffle=False, collate_fn=collate_fn)
+print("Train loader shape:", len(train_loader))
+print("Val loader shape:", len(val_loader))
+# Test load 1 batch
+images, targets = next(iter(train_loader))
+print("Batch image shape:", images[0].shape)
+print("Batch target example:", targets[0])
--- provision/urls/camera_routing.py
+++ provision/urls/camera_routing.py
@@ -2,5 +2,8 @@ from django.urls import re_path
 from provision.views.camera_views import CameraWebSocketView
 
 websocket_urlpatterns = [
+    # TODO: Authenticate client
+    # TODO: Stream video frames from server to client
+    # TODO: Write turn on/off funtionality for detection object in file path_to_file="view/camera_views.py"
     re_path(r"ws/camera/(?P<camera_id>\d+)/$", CameraWebSocketView.as_asgi()),
 ]
