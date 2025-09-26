7fd5c8f04447afc4d0972372d7cf24b21ab75f41
Nguyễn Văn Toản <95391834+vantoan2905@users.noreply.github.com>
2025-09-26 17:01:14 +0700

    Refactor project structure and remove obsolete files
    - Deleted GitHub Actions workflow for Django CI.
    - Updated initial migration to reflect new model structure and relationships.
    - Removed unnecessary migration files related to user ID alterations.
    - Consolidated camera, defect, and model-related migrations into a single migration file.
    - Added new models: Employee, Task, and their relationships.
    - Updated __init__.py files to include new models and views.
    - Modified chatbot consumer to include orchestration service.
    - Refactored test_gs.py to implement a real-time chatbot interaction instead of video streaming.
    - Recreated utils/__init__.py to include necessary utility functions.

--- provision/models/__init__.py
+++ provision/models/__init__.py
@@ -1,5 +1,8 @@
-from .camera import CameraInfo
-from .models import MLModel
+from .camera import Camera
+from .employee import Employee
+from .inspection import Inspection
+from .models_ai import Model
+from .product import Product
+from .task import Task
+from .status_type import ModelStatus, ModelType
 from .users import User
-
-
---
+++ provision/utils/__init__.py
@@ -0,0 +1,4 @@
+from .custom_exception_handler import *
+from .mail_manager import *
+from .snowflake import *
+from .token_manager import *
